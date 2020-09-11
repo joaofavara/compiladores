63e46af0ffe569bfcada9ad6c5147af0b5559111
unknown <joaopedro_favara@hotmail.com>
2020-09-11 10:39:05 -0300

    feat: exibe os valores depois de ler o codigo

--- src/mixin/lexico/trataPontuacao.js
+++ src/mixin/lexico/trataPontuacao.js
@@ -2,7 +2,6 @@
 const teste = require('./lexema.json');
 
 module.exports = (obj) => {
-  console.log('batatatatata: ', obj.file[obj.caracter]);
   const pont = obj.file[obj.caracter];
   const token = { lexema: '', simbolo: '' };
   obj.caracter += 1;
